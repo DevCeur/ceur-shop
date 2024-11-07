381216f67e9d7f686308eb3432264ac1495db5ff
Carlos Umaña <dev.carlos.umana@gmail.com>
2024-11-07 14:12:32 -0500

    [feat]: create basic shopping cart modal

--- app/components/product-card/product-card.tsx
+++ app/components/product-card/product-card.tsx
@@ -1,13 +1,13 @@
 export const ProductCard = () => {
   return (
-    <div className="group aspect-square md:aspect-auto bg-neutral-100 hover:bg-neutral-200 p-6 md:p-10 flex flex-col justify-between transition-all duration-150">
-      <div className="md:opacity-0 md:group-hover:opacity-100 transition-all duration-150">
+    <div className="group aspect-square md:aspect-auto bg-neutral-100 hover:bg-neutral-200 p-6 md:p-10  transition-all duration-150">
+      <div className="w-full h-full xl:opacity-0 xl:group-hover:opacity-100 flex flex-col justify-between transition-all duration-150">
         <h3 className="text-3xl font-medium uppercase">Product</h3>
-      </div>
 
-      <div className="md:opacity-0 md:group-hover:opacity-100 w-full flex justify-between items-center transition-all duration-150">
-        <button>Buy</button>
-        <button>Add to Cart</button>
+        <div className="w-full flex justify-between items-center transition-all duration-150">
+          <button>Buy</button>
+          <button>Add to Cart</button>
+        </div>
       </div>
     </div>
   );
--- app/components/shopping-cart/shopping-cart.tsx
+++ app/components/shopping-cart/shopping-cart.tsx
@@ -1,11 +1,59 @@
-import { ShoppingCart as ShoppingCartIcon } from "react-feather";
+import { useState } from "react";
+import { ShoppingCart as ShoppingCartIcon, X } from "react-feather";
+import { AnimatePresence, motion } from "framer-motion";
+import { Dialog, DialogPanel, DialogTitle } from "@headlessui/react";
 
 import { IconButton } from "../icon-button";
 
 export const ShoppingCart = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <>
-      <IconButton icon={ShoppingCartIcon} />
+      <IconButton icon={ShoppingCartIcon} onClick={() => setIsOpen(true)} />
+
+      <AnimatePresence>
+        {isOpen && (
+          <Dialog
+            static
+            open={isOpen}
+            onClose={() => setIsOpen(false)}
+            className="relative z-50"
+          >
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="fixed inset-0 bg-black/30"
+            />
+
+            <div className="fixed inset-0 flex w-screen justify-end">
+              <DialogPanel
+                as={motion.div}
+                initial={{ opacity: 0, x: 10 }}
+                animate={{ opacity: 1, x: 0 }}
+                exit={{ opacity: 0, x: 10 }}
+                className="w-full max-w-screen-sm bg-white flex flex-col"
+              >
+                <div className="w-full flex justify-between items-center p-8">
+                  <DialogTitle className="text-lg font-semibold uppercase">
+                    shopping_cart
+                  </DialogTitle>
+
+                  <IconButton icon={X} onClick={() => setIsOpen(false)} />
+                </div>
+
+                <div className="flex-1 p-8"></div>
+
+                <div className="w-full p-8 border-t border-t-neutral-200">
+                  <h3>checkout_information</h3>
+                  <span></span>
+                </div>
+              </DialogPanel>
+            </div>
+          </Dialog>
+        )}
+      </AnimatePresence>
     </>
   );
 };
