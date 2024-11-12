f7f962f542ed2e559a33fa282efefc92234cf21c
Carlos Umaña <dev.carlos.umana@gmail.com>
2024-11-12 00:09:46 -0500

    [feat]: improvements on UI and UX

--- app/components/product-card/product-card.tsx
+++ app/components/product-card/product-card.tsx
@@ -31,8 +31,8 @@ export const ProductCard = ({ product }: ProductCardProps) => {
   return (
     <div className="w-full aspect-square bg-neutral-900 p-6 md:p-8">
       <div className="w-full h-full flex flex-col justify-between">
-        <div className="flex justify-between items-start">
-          <div className="flex flex-col gap-2">
+        <div className="flex justify-between items-start gap-6">
+          <div className="w-full max-w-[275px] xl:max-w-full flex flex-col gap-3">
             <p className="text-2xl font-medium">{product.name}</p>
             <p className="text-sm text-neutral-500">{product.description}</p>
           </div>
